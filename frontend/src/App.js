f465706f75cbe2b3107c5cf272854137f6a10254
Your Namesaifxyzyz <saifmujeeb2603@gmail.com>
2024-06-05 23:18:57 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 
 import './App.css';
 
-import { Signup } from './Components/LoginSignup/Signup';
-import Login from './Components/LoginSignup/Login';
+import Signup from './Components/Pages/Signup';
+import Login from './Components/Pages/Login';
+import Intro from './Components/Pages/Intro';
+import Signupworker from './Components/Pages/Signupworker';
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App" >
       <Routes>
-        <Route path="/" element={<Login/>} exact />
+        <Route path="/" element={<Intro/>} exact />
+        <Route path="/Login" element={<Login/>} />
         <Route path="/Signup" element={<Signup/>} />
+        <Route path="/Signupworker" element={<Signupworker/>} />
         
       </Routes>
     </div>
